5750fb15e7f6df672f14de08252a8e11406c9e52
yu-000 <122270871+yu-000@users.noreply.github.com>
2023-10-09 11:15:31 +0900

    Update save_counter.php

--- save_counter.php
+++ save_counter.php
@@ -1,11 +1,32 @@
 <?php
+// データベースに接続
+$servername = "localhost";
+$username = "ユーザー名";
+$password = "パスワード";
+$dbname = "データベース名";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// 接続エラーの確認
+if ($conn->connect_error) {
+    die("接続エラー: " . $conn->connect_error);
+}
+
 // クライアントから送信されたデータを受け取る
 $data = json_decode(file_get_contents('php://input'));
 
 if ($data && isset($data->count)) {
-    // カウンターの値をデータベースやファイルに保存する処理を実行する
-    // ここではファイルに保存する例を示します
+    // カウンターの値をデータベースに保存する
     $count = $data->count;
-    file_put_contents('counter.txt', $count);
+    $sql = "UPDATE counter_table SET count = $count WHERE id = 1";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "カウンターの値が更新されました";
+    } else {
+        echo "エラー: " . $sql . "<br>" . $conn->error;
+    }
 }
+
+// データベース接続を閉じる
+$conn->close();
 ?>
